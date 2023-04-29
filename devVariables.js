8103253e7419eff5bca8632e82dd7d0a5523c851
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2023-04-29 16:16:48 +0100

    Split out functions

---
+++ devVariables.js
@@ -0,0 +1,21 @@
+module.exports = {
+    getCurrentImageVersions: `{
+                "obolnetwork/charon": "v0.15.0",
+                "sigp/lighthouse": "v4.0.2-rc.0",
+                "consensys/teku": "23.3.1",
+                "prom/prometheus": "v2.41.0",
+                "grafana/grafana": "9.3.2",
+                "prom/node-exporter": "v1.5.0",
+                "jaegertracing/all-in-one": "1.41.0"
+                }`,
+    getGitHubRepoURL: {
+        "obolnetwork/charon": "https://github.com/obolnetwork/charon",
+        "sigp/lighthouse": "https://github.com/sigp/lighthouse",
+        "consensys/teku": "https://github.com/consensys/teku",
+        "prom/prometheus": "https://github.com/prometheus/prometheus",
+        "grafana/grafana": "https://github.com/grafana/grafana",
+        "prom/node-exporter": "https://github.com/prometheus/node_exporter",
+        "jaegertracing/all-in-one": "https://github.com/jaegertracing/jaeger",
+    },
+    variable3: "value3",
+}
--- updateImageVersions.js
+++ updateImageVersions.js
@@ -5,6 +5,8 @@ const { execSync } = require("child_process")
 const fs = require("fs")
 const path = require("path")
 
+const devVariables = require("./devVariables")
+
 // **************************************
 // Clone a repository if it doesn't exist
 // **************************************
@@ -55,19 +57,9 @@ async function getFileFromURL(URL) {
 async function getCurrentImageVersions(fileContents) {
     const prompt = `Read this docker-compose.yml file and find all the images used and the default versions that have been set. Don't include any results that aren't directly images. Only respond with the result in a json object format. Provide no text other than the direct json object result so that I can parse your response directly in my node.js script.\n\n${fileContents}`
 
-    if (process.env.ENV == "dev") {
-        return `{
-                "obolnetwork/charon": "v0.15.0",
-                "sigp/lighthouse": "v4.0.2-rc.0",
-                "consensys/teku": "23.3.1",
-                "prom/prometheus": "v2.41.0",
-                "grafana/grafana": "9.3.2",
-                "prom/node-exporter": "v1.5.0",
-                "jaegertracing/all-in-one": "1.41.0"
-                }`
-    } else {
-        return await sendToOpenAI(prompt)
-    }
+    return process.env.ENV == "dev"
+        ? devVariables.getCurrentImageVersions
+        : await sendToOpenAI(prompt)
 }
 
 // ***********************************************
@@ -76,25 +68,9 @@ async function getCurrentImageVersions(fileContents) {
 async function getGitHubRepoURL(repoName) {
     prompt = `What is the GitHub repo name for the DockerHub image ${repoName}? Only respond with the GitHub URL, no other text.`
 
-    if (process.env.ENV == "dev") {
-        if (repoName == "obolnetwork/charon") {
-            return `https://github.com/obolnetwork/charon`
-        } else if (repoName == "sigp/lighthouse") {
-            return `https://github.com/sigp/lighthouse`
-        } else if (repoName == "consensys/teku") {
-            return `https://github.com/consensys/teku`
-        } else if (repoName == "prom/prometheus") {
-            return `https://github.com/prometheus/prometheus`
-        } else if (repoName == "grafana/grafana") {
-            return `https://github.com/grafana/grafana`
-        } else if (repoName == "prom/node-exporter") {
-            return `https://github.com/prometheus/node_exporter`
-        } else if (repoName == "jaegertracing/all-in-one") {
-            return `https://github.com/jaegertracing/jaeger`
-        }
-    } else {
-        return await sendToOpenAI(prompt)
-    }
+    return process.env.ENV == "dev"
+        ? devVariables.getGitHubRepoURL[repoName]
+        : await sendToOpenAI(prompt)
 }
 
 // ***************************************
@@ -113,11 +89,11 @@ async function getLatestImageVersion(repoName, currentImageVersion) {
         const redirectedUrl = response.headers.location
 
         // Ask ChatGPT to compare the current version with that URL to see which is newer
-        const prompt = `This is the version of ${repoName} found from GitHub:\n\n${redirectedUrl}\n\nThis is the version I found in the docker-compose.yml file:\n\n${currentImageVersion}\n\nWhich version is newer? The version found on GitHub might be in a slightly different format from the one in the docker-compose.yml file, so make sure your response is in the format found in the docker-compose.yml file. Don't provide any text other than the version number. If both are the same version, respond with only the word "SAME".\n\n`
+        const prompt = `This is the version of ${repoName} found from GitHub:\n\n${redirectedUrl}\n\nThis is the version I found in the docker-compose.yml file:\n\n${currentImageVersion}\n\nWhich version is newer? The version found on GitHub might be in a slightly different format from the one in the docker-compose.yml file, so make sure your response is in the format found in the docker-compose.yml file. Don't provide any text other than the version number. If both are the same version, respond with the version found in the docker-compose.yml file.\n\n`
 
         if (process.env.ENV == "dev") {
             if (repoName == "obolnetwork/charon") {
-                return `SAME`
+                return `v0.15.0`
             } else if (repoName == "sigp/lighthouse") {
                 return `v4.1.0`
             } else if (repoName == "consensys/teku") {
@@ -127,7 +103,7 @@ async function getLatestImageVersion(repoName, currentImageVersion) {
             } else if (repoName == "grafana/grafana") {
                 return `9.5.1`
             } else if (repoName == "prom/node-exporter") {
-                return `SAME`
+                return `v1.5.0`
             } else if (repoName == "jaegertracing/all-in-one") {
                 return `1.44.0`
             }
@@ -137,6 +113,122 @@ async function getLatestImageVersion(repoName, currentImageVersion) {
     } catch (error) {
         console.error(`Error: ${error.message}`)
     }
+}
+
+// *********************************************************
+// Update the docker-compose.yml file with new image versions
+// *********************************************************
+async function updateDockerComposeFile(
+    mainRepoPath,
+    repo,
+    latestImageVersion,
+    currentImageVersion,
+    composeFilePath,
+    composeFileContents
+) {
+    console.log(
+        `Updating ${repo} from ${currentImageVersion} to ${latestImageVersion}`
+    )
+
+    // Create and checkout a new branch for the update
+    const branchName = `update-${repo}-${latestImageVersion}`
+
+    execSync(
+        `cd ${mainRepoPath} && git checkout --quiet main && git checkout --quiet -b ${branchName}`,
+        {
+            stdio: "inherit",
+        }
+    )
+
+    // Make changes to the docker-compose.yml file
+    // This is simple line replacement for now to avoid returning the whole file
+    const prompt = `I have this docker-compose.yml file. I want to change the default version of the image ${repo} from ${currentImageVersion} to ${latestImageVersion}. Keep all the other content of the line identical, only change the version. I don't want you to return the entire file, because it's too big. Only respond with the number of spaces to indent the line and the contents of the changed line. Don't provide any text other than the number of spaces to indent the line and the contents of the changed line in the format:\n\n{"indentation": "<NUMBER_OF_SPACES>", "updatedLine": "{CHANGED_LINE_CONTENT"}\n\n${composeFileContents}`
+
+    let result
+    if (process.env.ENV == "dev") {
+        if (repo == "obolnetwork/charon") {
+            result = {
+                indentation: "2",
+                updatedLine:
+                    "image: obolnetwork/charon:${CHARON_VERSION:-v0.15.0}",
+            }
+        } else if (repo == "sigp/lighthouse") {
+            result = {
+                indentation: "4",
+                updatedLine:
+                    "image: sigp/lighthouse:${LIGHTHOUSE_VERSION:-v4.1.0}",
+            }
+        } else if (repo == "consensys/teku") {
+            result = {
+                indentation: "4",
+                updatedLine: "image: consensys/teku:${TEKU_VERSION:-23.4.0}",
+            }
+        } else if (repo == "prom/prometheus") {
+            result = {
+                indentation: "4",
+                updatedLine:
+                    "image: prom/prometheus:${PROMETHEUS_VERSION:-v2.43.0}",
+            }
+        } else if (repo == "grafana/grafana") {
+            result = {
+                indentation: "4",
+                updatedLine: "image: grafana/grafana:${GRAFANA_VERSION:-9.5.1}",
+            }
+        } else if (repo == "prom/node_exporter") {
+            result = {
+                indentation: "4",
+                updatedLine:
+                    "image: prom/node-exporter:${NODE_EXPORTER_VERSION:-v1.5.0}",
+            }
+        } else if (repo == "jaegertracing/all-in-one") {
+            result = {
+                indentation: "4",
+                updatedLine:
+                    "image: jaegertracing/all-in-one:${JAEGAR_VERSION:-1.44.0}",
+            }
+        } else {
+            return
+        }
+    } else {
+        result = await sendToOpenAI(prompt)
+    }
+
+    const indentationSpaces = " ".repeat(Number(result.indentation))
+    const updatedLine = indentationSpaces + result.updatedLine
+
+    const lines = composeFileContents.split("\n")
+    const targetLine = `image: ${repo}:`
+    for (let i = 0; i < lines.length; i++) {
+        if (
+            lines[i].includes(targetLine) &&
+            lines[i].includes(currentImageVersion)
+        ) {
+            lines[i] = updatedLine
+            break
+        }
+    }
+
+    const updatedData = lines.join("\n")
+    fs.writeFileSync(composeFilePath, updatedData)
+
+    // Commit the changes
+    execSync(
+        `cd ${mainRepoPath} && git add . && git commit --quiet -m "Update ${repo} to ${latestImageVersion}"`,
+        { stdio: "inherit" }
+    )
+
+    // Push the branch
+    // execSync(`cd ${mainRepoPath} && git push -u origin ${branchName}`, {
+    //     stdio: "inherit",
+    // })
+
+    // Create a PR
+    // Use the GitHub API to create a PR
+    // https://docs.github.com/en/rest/reference/pulls#create-a-pull-request
+    // Include a summary of the changes made in the description of the PR
+    // Include a summary of the release notes from the github release page in the description of the PR
+    // If I tag myself, will I be notified of the PR?
+    // Since I'm the one creating the PR, I don't think I'll be notified, but I want to be
 }
 
 // Check if a branch with the specified name exists
@@ -175,143 +267,34 @@ async function main() {
         "docker-compose.yml"
     )
 
+    // Get the current image versions
     const imageVersions = JSON.parse(
         await getCurrentImageVersions(composeFileContents)
     )
 
-    // TODO: Sub process for each update
-    // At this point, there could start to be errors (e.g. trying to update everything could cause an error if one fails)
-    // Not something to fix right now, but something to be aware of that it would be better to spin off independent processes for each update
-
+    // For each image, check if there is a newer version
+    // and if there is, update the docker-compose.yml file
     for (const repo in imageVersions) {
         const currentImageVersion = imageVersions[repo]
         const latestImageVersion = await getLatestImageVersion(
             repo,
             currentImageVersion
         )
+        const prExists = checkIfBranchExists(
+            mainRepoPath,
+            repo,
+            latestImageVersion
+        )
 
-        if (
-            latestImageVersion != "SAME" &&
-            latestImageVersion != currentImageVersion
-        ) {
-            // Check if a PR already exists for this update
-            const prExists = checkIfBranchExists(
+        if (latestImageVersion != currentImageVersion && !prExists) {
+            await updateDockerComposeFile(
                 mainRepoPath,
                 repo,
-                latestImageVersion
+                latestImageVersion,
+                currentImageVersion,
+                composeFilePath,
+                composeFileContents
             )
-
-            if (!prExists) {
-                console.log(
-                    `Updating ${repo} from ${currentImageVersion} to ${latestImageVersion}`
-                )
-
-                // Create and checkout a new branch for the update
-                const branchName = `update-${repo}-${latestImageVersion}`
-
-                execSync(
-                    `cd ${mainRepoPath} && git checkout --quiet main && git checkout --quiet -b ${branchName}`,
-                    {
-                        stdio: "inherit",
-                    }
-                )
-
-                // Make changes to the docker-compose.yml file
-                // This is simple line replacement for now to avoid returning the whole file
-                const prompt = `I have this docker-compose.yml file. I want to change the default version of the image ${repo} from ${currentImageVersion} to ${latestImageVersion}. Keep all the other content of the line identical, only change the version. I don't want you to return the entire file, because it's too big. Only respond with the number of spaces to indent the line and the contents of the changed line. Don't provide any text other than the number of spaces to indent the line and the contents of the changed line in the format:\n\n{"indentation": "<NUMBER_OF_SPACES>", "updatedLine": "{CHANGED_LINE_CONTENT"}\n\n${composeFileContents}`
-
-                let result
-                if (process.env.ENV == "dev") {
-                    if (repo == "obolnetwork/charon") {
-                        result = {
-                            indentation: "2",
-                            updatedLine:
-                                "image: obolnetwork/charon:${CHARON_VERSION:-v0.15.0}",
-                        }
-                    } else if (repo == "sigp/lighthouse") {
-                        result = {
-                            indentation: "4",
-                            updatedLine:
-                                "image: sigp/lighthouse:${LIGHTHOUSE_VERSION:-v4.1.0}",
-                        }
-                    } else if (repo == "consensys/teku") {
-                        result = {
-                            indentation: "4",
-                            updatedLine:
-                                "image: consensys/teku:${TEKU_VERSION:-23.4.0}",
-                        }
-                    } else if (repo == "prom/prometheus") {
-                        result = {
-                            indentation: "4",
-                            updatedLine:
-                                "image: prom/prometheus:${PROMETHEUS_VERSION:-v2.43.0}",
-                        }
-                    } else if (repo == "grafana/grafana") {
-                        result = {
-                            indentation: "4",
-                            updatedLine:
-                                "image: grafana/grafana:${GRAFANA_VERSION:-9.5.1}",
-                        }
-                    } else if (repo == "prom/node_exporter") {
-                        result = {
-                            indentation: "4",
-                            updatedLine:
-                                "image: prom/node-exporter:${NODE_EXPORTER_VERSION:-v1.5.0}",
-                        }
-                    } else if (repo == "jaegertracing/all-in-one") {
-                        result = {
-                            indentation: "4",
-                            updatedLine:
-                                "image: jaegertracing/all-in-one:${JAEGAR_VERSION:-1.44.0}",
-                        }
-                    } else {
-                        return
-                    }
-                } else {
-                    result = await sendToOpenAI(prompt)
-                }
-
-                const indentationSpaces = " ".repeat(Number(result.indentation))
-                const updatedLine = indentationSpaces + result.updatedLine
-
-                const lines = composeFileContents.split("\n")
-                const targetLine = `image: ${repo}:`
-                for (let i = 0; i < lines.length; i++) {
-                    if (
-                        lines[i].includes(targetLine) &&
-                        lines[i].includes(currentImageVersion)
-                    ) {
-                        lines[i] = updatedLine
-                        break
-                    }
-                }
-
-                const updatedData = lines.join("\n")
-                fs.writeFileSync(composeFilePath, updatedData)
-
-                // Commit the changes
-                execSync(
-                    `cd ${mainRepoPath} && git add . && git commit --quiet -m "Update ${repo} to ${latestImageVersion}"`,
-                    { stdio: "inherit" }
-                )
-
-                // Push the branch
-                // execSync(`cd ${mainRepoPath} && git push -u origin ${branchName}`, {
-                //     stdio: "inherit",
-                // })
-
-                // Create a PR
-                // Use the GitHub API to create a PR
-                // https://docs.github.com/en/rest/reference/pulls#create-a-pull-request
-                // Include a summary of the changes made in the description of the PR
-                // Include a summary of the release notes from the github release page in the description of the PR
-                // If I tag myself, will I be notified of the PR?
-                // Since I'm the one creating the PR, I don't think I'll be notified, but I want to be
-            } else {
-                console.log(
-                    `A PR for updating ${repo} to ${latestImageVersion} already exists`
-                )
-            }
         }
     }
 }
